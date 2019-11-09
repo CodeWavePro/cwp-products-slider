83c7583265a60240786a970727b22fd192f49208
Critick <andrew.stezenko@gmail.com>
2019-11-09 20:39:21 +0300

    9.11.19

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ $cfg = array();
 $cfg['page_builder'] = array(
 	'title'			=> esc_html__( 'CWP Products Slider', 'mebel-laim' ),
 	'description'	=> esc_html__( 'Add slider with settings.', 'mebel-laim' ),
-	'tab'			=> esc_html__( 'Media Elements', 'mebel-laim' ),
+	'tab'			=> esc_html__( 'Content Elements', 'mebel-laim' ),
 	'icon' 			=> 'dashicons dashicons-format-gallery'
 );
