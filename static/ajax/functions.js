5fca7eda975c8094f6ca491deaa8f11326828e72
Critick <andrew.stezenko@gmail.com>
2019-11-16 01:59:56 +0300

    Still don't working... >_<

--- static/ajax/functions.js
+++ static/ajax/functions.js
@@ -24,91 +24,76 @@ jQuery( function( $ ) {
 				};
 				console.log( 'before ajax' );
 
-				$.ajax ( {
-					type 		: 'POST',
-		            url 		: cwpAjax.ajaxurl,
-		            data 		: data,
-		            dataType	: 'json'
-				} )
-				
-				.done( function( html ) {
-					console.log( html );
-				} )
+				$.post( cwpAjax.ajaxurl, data, function( data ) {	// Ajax post request.
+					console.log( 'after ajax' );
+					switch ( data.success ) {	// Checking ajax response.
+						case true: 	// If ajax response is success.
+							/**
+							 * Filling all more product info fields with response data.
+							 */
+							if ( data.data.thumbnail != '' ) {	// If thumbnail is not empty.
+								$( '.cwp-more-info__image' ).html( data.data.thumbnail );
+							}
 
-				.fail( function( jqXHR, textStatus, errorThrown ) {
-					console.log( String( errorThrown ) );
+							if ( data.data.old_price != '' ) {	// If old price is not empty.
+								$( '.cwp-more-info-prices__old' ).html( data.data.old_price );
+							}
+
+							if ( data.data.new_price != '' ) {	// If new price is not empty.
+								$( '.cwp-more-info-prices__new' ).html( data.data.new_price );
+							}
+
+							if ( data.data.type != '' ) {	// If type is not empty.
+								$( '.cwp-more-info-type' ).html( data.data.type );
+							}
+
+							if ( data.data.material != '' ) {	// If new price is not empty.
+								$( '.cwp-more-info-material' ).html( data.data.material );
+							}
+
+							if ( data.data.width != '' ) {	// If width is not empty.
+								$( '.cwp-more-info-width' ).html( data.data.width );
+							}
+
+							if ( data.data.height != '' ) {	// If height is not empty.
+								$( '.cwp-more-info-height' ).html( data.data.height );
+							}
+
+							if ( data.data.depth != '' ) {	// If depth is not empty.
+								$( '.cwp-more-info-depth' ).html( data.data.depth );
+							}
+
+							if ( data.data.text != '' ) {	// If text is not empty.
+								$( '.cwp-more-info-text' ).html( data.data.text );
+							}
+
+							if ( data.data.per_pack != '' ) {	// If number of products per pack is not empty.
+								$( '.cwp-more-info-number-per-pack' ).html( data.data.per_pack );
+							}
+
+							if ( data.data.manufacture != '' ) {	// If manufacture country is not empty.
+								$( '.cwp-more-info-manufacture-country' ).html( data.data.manufacture );
+							}
+
+							if ( data.data.brand != '' ) {	// If brand country is not empty.
+								$( '.cwp-more-info-brand-country' ).html( data.data.brand );
+							}
+
+							if ( data.data.guarantee != '' ) {	// If depth is not empty.
+								$( '.cwp-more-info-guarantee' ).html( data.data.guarantee );
+							}
+
+							console.log( data.data.message );	// Show success message in console.
+			    			break;
+
+						case false: 	// If we have some errors.
+			    			console.log( data.data.message );	// Show errors in console.
+
+			    		default: 	// Default variant.
+			    			console.log( 'Unknown error!' );	// Show message of unknown error in console.
+			    			break;
+					}
 				} );
-
-				// $.post( cwpAjax.ajaxurl, data, function( data ) {	// Ajax post request.
-				// 	console.log( 'after ajax' );
-				// 	switch ( data.success ) {	// Checking ajax response.
-				// 		case true: 	// If ajax response is success.
-				// 			/**
-				// 			 * Filling all more product info fields with response data.
-				// 			 */
-				// 			if ( data.data.thumbnail != '' ) {	// If thumbnail is not empty.
-				// 				$( '.cwp-more-info__image' ).html( data.data.thumbnail );
-				// 			}
-
-				// 			if ( data.data.old_price != '' ) {	// If old price is not empty.
-				// 				$( '.cwp-more-info-prices__old' ).html( data.data.old_price );
-				// 			}
-
-				// 			if ( data.data.new_price != '' ) {	// If new price is not empty.
-				// 				$( '.cwp-more-info-prices__new' ).html( data.data.new_price );
-				// 			}
-
-				// 			if ( data.data.type != '' ) {	// If type is not empty.
-				// 				$( '.cwp-more-info-type' ).html( data.data.type );
-				// 			}
-
-				// 			if ( data.data.material != '' ) {	// If new price is not empty.
-				// 				$( '.cwp-more-info-material' ).html( data.data.material );
-				// 			}
-
-				// 			if ( data.data.width != '' ) {	// If width is not empty.
-				// 				$( '.cwp-more-info-width' ).html( data.data.width );
-				// 			}
-
-				// 			if ( data.data.height != '' ) {	// If height is not empty.
-				// 				$( '.cwp-more-info-height' ).html( data.data.height );
-				// 			}
-
-				// 			if ( data.data.depth != '' ) {	// If depth is not empty.
-				// 				$( '.cwp-more-info-depth' ).html( data.data.depth );
-				// 			}
-
-				// 			if ( data.data.text != '' ) {	// If text is not empty.
-				// 				$( '.cwp-more-info-text' ).html( data.data.text );
-				// 			}
-
-				// 			if ( data.data.per_pack != '' ) {	// If number of products per pack is not empty.
-				// 				$( '.cwp-more-info-number-per-pack' ).html( data.data.per_pack );
-				// 			}
-
-				// 			if ( data.data.manufacture != '' ) {	// If manufacture country is not empty.
-				// 				$( '.cwp-more-info-manufacture-country' ).html( data.data.manufacture );
-				// 			}
-
-				// 			if ( data.data.brand != '' ) {	// If brand country is not empty.
-				// 				$( '.cwp-more-info-brand-country' ).html( data.data.brand );
-				// 			}
-
-				// 			if ( data.data.guarantee != '' ) {	// If depth is not empty.
-				// 				$( '.cwp-more-info-guarantee' ).html( data.data.guarantee );
-				// 			}
-
-				// 			console.log( data.data.message );	// Show success message in console.
-			 //    			break;
-
-				// 		case false: 	// If we have some errors.
-			 //    			console.log( data.data.message );	// Show errors in console.
-
-			 //    		default: 	// Default variant.
-			 //    			console.log( 'Unknown error!' );	// Show message of unknown error in console.
-			 //    			break;
-				// 	}
-				// } );
 
 				$( '.cwp-more-info-wrapper' ).css( 'display', 'block' );	// Show more info block.
 				isActiveAjax = false;	// User can use ajax ahead.
