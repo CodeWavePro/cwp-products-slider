2f453720f082bab2e05d67a690c23db4c24e272f
Critick <andrew.stezenko@gmail.com>
2019-11-25 18:26:07 +0300

    Add local Owl Carousel 2 files.

--- static.php
+++ static.php
@@ -26,5 +26,33 @@ if ( !is_admin() ) {
 			array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) )
 		);
 	}
+
+	// Check if Owl Carousel 2 script and styles are registered (enqueued).
+	if ( wp_script_is( 'cwp-owl-carousel-2-script', 'queue' ) ) {
+		return false;
+	}	else {
+		wp_enqueue_script(
+			'cwp-owl-carousel-2-script',
+			$uri . '/static/libs/js/owl.carousel.min.js',
+			array( 'jquery' )
+		);
+	}
+
+	if ( wp_style_is( 'cwp-owl-carousel-2-css', 'queue' ) ) {
+		return false;
+	}	else {
+		wp_enqueue_style(
+			'cwp-owl-carousel-2-css',
+			$uri . '/static/libs/css/owl.carousel.min.css'
+		);
+	}
+
+	if ( wp_style_is( 'cwp-owl-carousel-2-theme-css', 'queue' ) ) {
+		return false;
+	}	else {
+		wp_enqueue_style(
+			'cwp-owl-carousel-2-theme-css',
+			$uri . '/static/libs/css/owl.theme.default.min.css'
+		);
+	}
 }
-?> 
